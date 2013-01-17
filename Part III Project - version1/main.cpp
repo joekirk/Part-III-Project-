7c5e65aec7511cdaf5a9631fe50504811c416d60
Joe Kirk <joekirk@Joes-MacBook-Pro.local>
2013-01-17 21:09:49 +0000

    Initial Commit

---
+++ Part III Project - version1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Part III Project - version1
+//
+//  Created by Joe Kirk on 17/01/2013.
+//  Copyright (c) 2013 Joe Kirk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
